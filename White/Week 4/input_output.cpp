834ae3c205a8ebf7d444688b3c917a5997279a3c
AlexanderStasiuk <masherov14@tut.by>
2020-08-07 18:26:28 +0300

    Week 4. File streams

---
+++ White/Week 4/input_output.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<fstream>
+using namespace std;
+
+
+int main() {
+    ifstream input("input.txt");
+    ofstream output("output.txt");
+    string line;
+    if (input.is_open()){
+        while (getline(input, line)){
+            output << line << endl;
+        }
+    }
+    return 0;
+}
