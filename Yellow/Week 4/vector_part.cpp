93f0142b1f5e03e4f75657becda7c3affbb94173
AlexanderStasiuk <masherov14@tut.by>
2020-08-27 01:28:53 +0300

    Week 4. Iterators. Vector part

---
+++ Yellow/Week 4/vector_part.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+void PrintVectorPart(const vector<int>& numbers) {
+    auto it = find_if(
+        begin(numbers), end(numbers), 
+        [](const int& el) {
+            return el < 0;
+        }
+    );
+    while(it != begin(numbers)) {
+        it--;
+        cout << *it << " ";
+    }
+}
+
+int main() {
+    PrintVectorPart({6, 1, 8, -5, 4});
+    cout << endl;
+    PrintVectorPart({-6, 1, 8, -5, 4});  // ничего не выведется
+    cout << endl;
+    PrintVectorPart({6, 1, 8, 5, 4});
+    cout << endl;
+    return 0;
+}
