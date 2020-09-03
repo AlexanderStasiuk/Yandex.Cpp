648aaf6398b93280130136bfcbf8435a11763e18
AlexanderStasiuk <masherov14@tut.by>
2020-09-03 15:33:57 +0300

    Week5. Inheritence

---
+++ Yellow/Week 5/inheritence.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+class Animal {
+public:
+    Animal(string name): Name(name) {
+    }
+    const string Name;
+};
+
+
+class Dog : public Animal {
+public:
+    Dog(string name): Animal(name) {
+    }
+
+    void Bark() {
+        cout << Name << " barks: woof!" << endl;
+    }
+};
+
+int main() {
+    Dog d("Jim");
+    d.Bark();
+    return 0;
+}
