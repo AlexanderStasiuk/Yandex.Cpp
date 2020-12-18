97d6983db861678774eaf22c8a8c81a39898189d
AlexanderStasiuk <masherov14@tut.by>
2020-12-18 23:08:25 +0300

    Brown. Final Project. D

--- Brown/Week 6. Final Project/main.cpp
+++ Brown/Week 6. Final Project/main.cpp
@@ -2,6 +2,7 @@
 #include "test_runner.h"
 
 #include "request_handler.h"
+#include "json.h"
 
 #include <string>
 #include <iostream>
@@ -9,8 +10,10 @@
 using namespace std;
 
 int main() {
-    auto manager = buildDataBase();
-    processRequests(manager);
+    auto doc = Json::Load(std::cin);
+    const auto& rootMap = doc.GetRoot().AsMap();
+    auto manager = buildDataBase(rootMap.at("base_requests").AsArray());
+    processRequests(manager, rootMap.at("stat_requests").AsArray());
 
     return 0;
 }
